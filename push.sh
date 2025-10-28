505970298f2afce13ca61815be33f8a10b640dd3
giliam <matthieu.denoux@gmail.com>
2025-10-28 23:33:56 +0100

    Better version

--- push.sh
+++ push.sh
@@ -1,4 +1,16 @@
-./move.sh $2
+source="${2:-notes}"
+echo "Moving according to parameter ${source}"
+if [ "$source" = "pdf" ]
+then
+    echo "...going to /storage/emulated/0/note/"
+    cd /storage/emulated/0/note/
+else
+    echo "...going to ~/storage/documents/NewNanowrimo/"
+    cd ~/storage/documents/NewNanowrimo/
+fi
+
+echo "Arrived in folder:"
+pwd
 
 echo "Adding, committing and pushing"
 echo "--> Message: $1"
